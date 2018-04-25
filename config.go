66353b600ba516c518d9032679555c506df33ea6
Vallery Lancey <vallerylancey@gmail.com>
2018-04-24 21:35:04 -0700

    Added some skeletal content.

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+const (
+	defaultListenPort   = "50052"
+	defaultRedisAddress = "localhost"
+	defaultRedisPort    = "6379"
+)
+
+// Define config globals.
+var listenPort = ""
+var redisAddress = ""
+var redisPort = ""
+
+func loadConfigGlobals() {
+	listenPort = os.Getenv("FFLAG_MANAGER_PORT")
+	if listenPort == "" {
+		listenPort = defaultListenPort
+	}
+
+	redisAddress = os.Getenv("FFLAG_MASTER_REDIS_ADDRESS")
+	if redisAddress == "" {
+		redisAddress = defaultRedisAddress
+	}
+
+	redisPort = os.Getenv("FFLAG_MASTER_REDIS_PORT")
+	if redisPort == "" {
+		redisPort = defaultRedisPort
+	}
+
+	fmt.Println("Loaded config - Redis on ", redisAddress, ":", redisPort)
+}
